9f3d93bd6ea2b7526138d396c72e87c36ba0abbb
Arne Martin Aurlien <am@aurlien.net>
2011-06-27 13:40:02 +0200

    Actually use initial offset Update demo

--- flickable.js
+++ flickable.js
@@ -41,14 +41,14 @@ var Flickable = function(elementSelector, options) {
                 item = element.querySelector(settings.itemSelector),
                 subItemCount = item.children.length,
                 offset = settings.offset;
-
+            
             // Set up default styles
-            item.style.WebkitTransform = 'translate3d(0, 0, 0)';
-            item.style.MozTransform = 'translateX(0)';
-            item.style.OTransform = 'translateX(0)';
-            item.style.transform = 'translateX(0)';
+            item.style.WebkitTransform = 'translate3d(' + offset + 'px, 0, 0)';
+            item.style.MozTransform = 'translateX(' + offset + 'px)';
+            item.style.OTransform = 'translateX(' + offset + 'px)';
+            item.style.transform = 'translate3d(' + offset + 'px, 0, 0)';
             item.style.width = (settings.itemWidth * subItemCount) + 'px';
-
+            
             // Get X and Y value from a touch or mouse event
             var getXY = function(evt) {
                 if (evt.touches && evt.touches.length) {
@@ -57,61 +57,61 @@ var Flickable = function(elementSelector, options) {
                     return [evt.clientX, evt.clientY];
                 }
             };
-
+            
             // Set up listener
             element.addEventListener(events.start, function(evt) {
-
+                
                 // Get origin position
                 var origin = getXY(evt);
                 var current = origin;
-
+                
                 // Disable animations while dragging
                 item.style.WebkitTransition = '';
                 item.style.MozTransition = '';
                 item.style.OTransition = '';
                 item.style.transition = '';
-
+                
                 // Reposition gallery based on event
                 var reposition = function(evt) {
                     var distanceX = Math.abs(current[0] - origin[0]);
                     var distanceY = Math.abs(current[1] - origin[1]);
-
+                    
                     // Only scroll gallery if X distance is greater than Y distance
                     if (distanceX > distanceY) {
                         evt.preventDefault();
                     } else {
                         current[0] = origin[0];
                     }
-
+                    
                     // Get delta X distance
                     var delta = current[0] - origin[0];
-
+                    
                     // Make scrolling "sticky" if we are scrolling past the first or last panel
                     if (offset + delta > 0 || offset + delta < -((subItemCount-1) * settings.itemWidth)) {
                         delta = Math.floor(delta / 2);
                     }
-
+                    
                     // Update position
                     item.style.WebkitTransform = 'translate3d(' + (offset + delta) + 'px, 0, 0)';
                     item.style.MozTransform = 'translateX(' + (offset + delta) + 'px)';
                     item.style.OTransform = 'translateX(' + (offset + delta) + 'px)';
-                    item.style.transform = 'translateX(' + (offset + delta) + 'px)';
+                    item.style.transform = 'translate3d(' + (offset + delta) + 'px, 0, 0)';
                 };
-
+                
                 var moveEvent = function(evt) {
                     current = getXY(evt);
                     reposition(evt);
                 };
-
+                
                 var endEvent = function(evt) {
                     var diff = current[0] - origin[0];
-
+                    
                     // Enable animation
                     item.style.WebkitTransition = '-webkit-transform 0.4s ease';
                     item.style.MozTransition = '-moz-transform 0.4s ease';
                     item.style.OTransition = '-o-transform 0.4s ease';
                     item.style.transition = 'transform 0.4s ease';
-
+                    
                     // Snap to closest panel
                     if (diff > settings.itemWidth / 4 && offset !== 0) {
                         current[0] = origin[0] + settings.itemWidth;
@@ -125,16 +125,16 @@ var Flickable = function(elementSelector, options) {
                         current[0] = origin[0];
                         reposition(evt);
                     }
-
+                    
                     // Remove drag and end event listeners
                     element.removeEventListener(events.move, moveEvent, false);
                     element.removeEventListener(events.end, endEvent, false);
                 };
-
+                
                 // Set up drag and end event listeners
                 element.addEventListener(events.move, moveEvent, false);
                 element.addEventListener(events.end, endEvent, false);
-
+                
             }, false);
         })();
     }
